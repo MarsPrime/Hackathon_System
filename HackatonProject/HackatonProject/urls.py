8931c5e10eea5374fdc3e032b034e8b425320115
ilya_sobolev <ilyasobolev750@gmail.com>
2025-11-22 15:16:06 +0700

    Fix views import in urls.py

--- HackatonProject/HackatonProject/urls.py
+++ HackatonProject/HackatonProject/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import home
-
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_page_view, name='home_page'),
+    path('', views.home_page_view, name='home_page'),
 ]
