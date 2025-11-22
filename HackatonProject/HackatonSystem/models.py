f58f6ea4f4215557c6ecb2a9da9969c985dc9c3c
demid <demid090605@outlook.com>
2025-11-22 16:12:14 +0700

    add base roles 	изменено:      HackatonSystem/__pycache__/views.cpython-313.pyc 	изменено:      HackatonSystem/models.py

--- HackatonProject/HackatonSystem/models.py
+++ HackatonProject/HackatonSystem/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=128)
+    femail = models.CharField(max_length=128)
+    patronymic = models.CharField(max_length=128)
+    phone = models.IntegerField(max_length=11)
+    email = models.EmailField()
+
+    login = models.CharField(max_length=64)
+    password = models.CharField(max_length=64)
+
+class Group(models.Model):
+    name = models.CharField(max_length=128)
+    event = models.IntegerField()
+
+class Event(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    place = models.TextField()
+    date = models.DateField()
